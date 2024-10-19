4c290f88b4472b38982adc0dcbb34e6dfae5f42a
Aksa-Rose <aksarose3@gmail.com>
2024-10-19 22:02:13 +0530

    build:setup base

---
+++ hoodmap-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MapComponent from './MapComponent';
+import './index.css';
+
+function App() {
+    return (
+        <div>
+            <MapComponent />
+        </div>
+    );
+}
+
+export default App;
+
